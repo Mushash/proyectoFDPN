309c38402cccca361bcb95a4353b2c484715671a
gmail <diego.ruiz1826@alumnos.udg.mx>
2024-09-18 11:16:48 -0600

    p6 signup

--- fdpn.py
+++ fdpn.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template,url_for
 from config import config
 
-fdpnApp = Flask("/")
+fdpnApp = Flask(__name__)
+
+@fdpnApp.router('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
+@fdpnApp.router('/signup')
+def signup():
+    return render_template('signup.html')
+
+@fdpnApp.router('/signin')
+def signin():
+    return render_template('signin.html')
 
 if __name__=="_main_":
     fdpnApp.run(debug=True,port=3300)
