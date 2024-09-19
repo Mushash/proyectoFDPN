ca046f8c0e1b8486199457055dddb3d37ff33334
romanmendoza <angel.mendoza5869@alumnos.udg.mx>
2024-09-19 08:39:32 -0600

    p7 user

--- fdpn.py
+++ fdpn.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for 
+from werkzeug.security import generate_password_hash
+import datetime
 from config import config
 
 fdpnApp = Flask(__name__)
@@ -9,6 +11,13 @@ def home():
 
 @fdpnApp.router('/signup')
 def signup():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        correo = request.form['correo']
+        clave = request.form['clave']
+        claveCifrada = generate_password_hash(clave)
+        fechare = datetime.datetime()
+        regUsuario = db.conection.cursor()
     return render_template('signup.html')
 
 @fdpnApp.router('/signin')
